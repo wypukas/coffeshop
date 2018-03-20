f35f943c942e26995cea2607721d497c00a2e8f8
wypukas <zilvinas@Zilvinass-MacBook-Pro.local>
2018-03-20 19:39:39 +0200

    Coffee shop template coding

--- dist/components/head.php
+++ dist/components/head.php
@@ -2,7 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="description" content="Hot coffee">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Coffee shop</title>
+    <link rel="stylesheet" href="web-fonts-with-css/css/fontawesome-all.min.css">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="img/icon.png">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 </head>
